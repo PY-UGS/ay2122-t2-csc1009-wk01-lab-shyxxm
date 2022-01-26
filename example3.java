3b60b6d5b297e296cc8c030430433b820040946e
shyxxm <48471694+shyxxm@users.noreply.github.com>
2022-01-26 11:24:22 +0800

    Add files via upload

---
+++ example3.java
@@ -0,0 +1,15 @@
+class example3 {
+    public static void main(String args[])
+    {
+        for(int x=102; x > 66; x=x-1)
+        {
+            if (x%2==0)
+            {
+                
+            }
+            else{
+                System.out.println("value of x:" + x);
+            }
+        }
+    }
+}
